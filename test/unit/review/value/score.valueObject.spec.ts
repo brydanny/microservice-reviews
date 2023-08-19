319e1e1fa0edc27cacae12e09a690862d288309d
Danny Chambi <brydanny@gmail.com>
2023-08-18 23:30:53 -0400

    definicion de pruebas unitarias

---
+++ test/unit/review/value/score.valueObject.spec.ts
@@ -0,0 +1,36 @@
+import { Score } from '../../../../src/reviews/domain/valueObjects/score.valueObject';
+
+describe('Score ValueObject', () => {
+  test('Creacion e score con la validad minima de cero', () => {
+    const actualScore: Score = new Score();
+
+    expect(actualScore.getScore()).toBe(0);
+  });
+
+  test('Se e score con la validad minima de cero', () => {
+    const actualScore: Score = new Score();
+    actualScore.setScore(5);
+
+    expect(actualScore.getScore()).toBe(5);
+  });
+  test('score invalido', () => {
+    try {
+      const actualScore: Score = new Score();
+      actualScore.setScore(-1);
+    } catch (error) {
+      expect(() => {
+        throw error;
+      }).toThrowError('score invalido');
+    }
+  });
+  test('score invalido mayor a 10', () => {
+    try {
+      const actualScore: Score = new Score();
+      actualScore.setScore(11);
+    } catch (error) {
+      expect(() => {
+        throw error;
+      }).toThrowError('score invalido');
+    }
+  });
+});
