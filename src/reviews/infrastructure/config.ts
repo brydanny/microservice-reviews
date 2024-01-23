a19629c57b8c76867dec0313787447428b118728
Danny Chambi <brydanny@gmail.com>
2024-01-23 12:11:49 -0400

    ajustes config mongo

--- src/reviews/infrastructure/config.ts
+++ src/reviews/infrastructure/config.ts
@@ -4,9 +4,9 @@ export default registerAs('config', () => {
   return {
     mongo: {
       dbName: process.env.MONGO_DB || 'review-db',
-      user: process.env.MONGO_INITDB_ROOT_USERNAME || 'root',
-      password: process.env.MONGO_INITDB_ROOT_PASSWORD || 'root',
-      port: parseInt(process.env.MONGO_PORT, 10) || 27017,
+      user: process.env.MONGO_INITDB_ROOT_USERNAME || 'mongo',
+      password: process.env.MONGO_INITDB_ROOT_PASSWORD || 'secret',
+      port: parseInt(process.env.MONGO_PORT, 10) || 27018,
       host: process.env.MONGO_HOST || 'localhost',
       connection: process.env.MONGO_CONNECTION || 'mongodb',
     },
